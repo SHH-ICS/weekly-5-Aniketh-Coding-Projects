2e23dc9578bee2c4c7561916bc14497132c7cda9
Aniketh Pamu <aniketh.pamu@stu.ocsb.ca>
2024-11-29 15:04:33 +0000

    FINAL COMMIT PYTHON

--- assignment.py
+++ assignment.py
@@ -1,11 +1,10 @@
-    iterations = input()
+iterations = input()
 
-    if iterations.isdigit() and int(iterations) > 0:
+if iterations.isdigit() and int(iterations) > 0:
         iterations = int(iterations)
         pi = 0
-        for i in range(iterations)
-            pi += (-1)**1 / (2 * i + 1)
+        for i in range(iterations):
+            pi += (-1)**i / (2 * i + 1)
         print(4 * pi)
-    else:
-        print("Error)
-    
+else:
+        print("Error")
